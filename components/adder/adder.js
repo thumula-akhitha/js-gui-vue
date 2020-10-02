82449bbc6df95875e99d8ab8a5bbb84059dd6aee
thumula-akhitha <54450547+thumula-akhitha@users.noreply.github.com>
2020-10-02 12:05:02 -0500

    added thirdNumber in adder.js file

--- components/adder/adder.js
+++ components/adder/adder.js
@@ -1,17 +1,19 @@
-const add = (x, y) => { return x + y }
+const add = (x, y, z) => { return x + y + z }
 
 const adder = new Vue({
   el: '#adder',
   data: {
     guest: 'Emmett',
     firstNumber: 5,
-    secondNumber: 3
+    secondNumber: 3,
+    thirdNumber: 2
   },
   computed: {
     result: function () {
       const i = parseInt(this.firstNumber)
       const j = parseInt(this.secondNumber)
-      return `${this.guest}, your sum is ${add(i, j)}.`
+      const z = parseInt(this.thirdNumber)
+      return `${this.guest}, your sum is ${add(i, j, z)}.`
     }
   }
 })
